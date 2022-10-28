70a95afcbdae77811f1f96ba084707696e2abdef
Decoders <gonzgonc@gmail.com>
2022-10-27 21:49:18 -0500

    Header agregado

--- front/src/App.js
+++ front/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        {/* <Header/> */}
+        <Header/>
         <div className='container container-fluid'>
           <Routes>
             <Route path='/Home' element={<Home />} />
