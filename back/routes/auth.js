9abf154a48c7cf6950460d47dcf14d0ee04c154d
Paula Andrea Gonzalez H <paulaandrea0316@yahoo.com.mx>
2022-11-10 22:21:31 -0500

    middleware-routes-auth-authController
    Se capturan los errores en el middleware de clave duplicada-y token espirado
    Se adicionan los controladores para saber quien esta logueado, update contraseña, comparar contraseñas, actualizar perfil y controladores para el usuario admin

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -3,7 +3,14 @@ const { registroUsuario,
         loginUser,
         logOut,
         forgotPassword,
-        resetPassword
+        resetPassword,
+        getUserProfile,
+        updatePassword,
+        updateProfile,
+        getAllUsers,
+        getUserDetails,
+        updateUser,
+        deleteUser
  } = require("../controllers/authController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router= express.Router();
@@ -14,6 +21,15 @@ router.route('/login').get(loginUser)
 router.route('/logout').get(isAuthenticatedUser, logOut)
 router.route("/forgotPassword").post(forgotPassword)
 router.route('/resetPassword/:token').post(resetPassword)
+router.route('/yo').get(isAuthenticatedUser, getUserProfile)
+router.route('/yo/updatePassword').put(isAuthenticatedUser, updatePassword)
+router.route('/yo/updateProfile').put(isAuthenticatedUser, updateProfile)
 
+
+//rutas admin
+router.route('/admin/allUsers').get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers)
+router.route('/admin/user/:id').get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails)
+router.route('/admin/updateUser/:id').put(isAuthenticatedUser, authorizeRoles("admin"), updateUser)
+router.route("/admin/deleteUser/:id").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser)
 
 module.exports= router
