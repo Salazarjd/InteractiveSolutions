90b03761d4f70210c0c143c406d94cff04f36714
Juan Diego Salazar Velasquez <salazar.jd@gmail.com>
2022-11-17 18:29:00 -0500

    Register funcional

--- back/app.js
+++ back/app.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 const Server = require('./models/server');
-
+const cloudinary = require('cloudinary');
 
 const server = new Server();
+
+//Configurar cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_APY_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 server.listen();
